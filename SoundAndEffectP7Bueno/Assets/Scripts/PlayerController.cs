74dec645c9deaec9d3897957d245ea316574a107
j9rely <145072305+j9rely@users.noreply.github.com>
2024-04-18 15:28:24 -0700

    ffinished
    dine

--- SoundAndEffectP7Bueno/Assets/Scripts/PlayerController.cs
+++ SoundAndEffectP7Bueno/Assets/Scripts/PlayerController.cs
@@ -16,7 +16,9 @@ public class PlayerController : MonoBehaviour
     public ParticleSystem dirtParticle;
     public bool gameOver = false;
     public ParticleSystem explosionParticle;
-
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +35,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true; playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false; playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+        }
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerAudio.PlayOneShot(jumpSound, 1.0f);
@@ -40,6 +50,15 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
--- SoundAndEffectP7Bueno/Assets/Scripts/SpawnManager.cs
+++ SoundAndEffectP7Bueno/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] obstaclePrefabs;
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    private int randomObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,20 @@ public class SpawnManager : MonoBehaviour
             GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    void SpawnObstacle ()
+    void SpawnObstacle()
     {
-        if(playerControllerScript.gameOver == false)
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-    }
+        if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle],
+                spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
